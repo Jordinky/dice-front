49d1ef99244cfd03741ea30bfded29830778faa3
ghOdisea <154799410+ghOdisea@users.noreply.github.com>
2024-06-12 12:53:29 +0200

    rankings lists + All players list

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import ThemeToggle from './components/ThemeToggle';
 import { Container, Box } from '@mui/material';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import Ranking from './components/Ranking';
+import Ranking from './components/Rankings/Ranking';
+import RankingWinners from './components/Rankings/RankingWinners';
+import RankingLosers from './components/Rankings/RankingLosers';
+import AllPlayers from './components/Players/AllPlayers';
 function App() {
   return (
     <ThemeToggle>
       <Navbar/>
       <Container component="main" sx={{ mt: 8, mb: 2 }}>
         <Box sx={{ my: 4 }}>
-          <Ranking />
+          {/* <Ranking />
+          <RankingWinners />
+          <RankingLosers /> */}
+          <AllPlayers />
         </Box>
       </Container>
       <Footer />
--- src/components/Rankings/Ranking.jsx
+++ src/components/Rankings/Ranking.jsx
@@ -40,7 +40,7 @@ export default function Ranking(){
         </div> */}
 
     <div>
-      <h1>Ranking de jugadores </h1>
+      <h1>Ranking total de jugadores 🏁</h1>
       <TableContainer component={Paper}>
     <Table sx={{ minWidth: 650 }} aria-label="simple table">
       <TableHead>
@@ -70,49 +70,3 @@ export default function Ranking(){
   </>
   )
 }
-
-
-// function createData(Player_name, Player_ID, totalGames, succes_rate) {
-//   return { Player_name, Player_ID, totalGames, succes_rate };
-// }
-
-// const rows = [//   return { Player_name, Player_ID, totalGames, succes_rate};
-//   // // }
-//   createData('Player 1', 1, 0, 100),
-//   createData('Player 2', 2, 0, 50),
-//   createData('Player 3', 3, 0, 60),
-//   createData('Player 4', 4, 0, 75),
-//   createData('Player 5', 5, 0, 33),
-// ];
-
-// export default function BasicTable() {
-//   return (
-//     <TableContainer component={Paper}>
-//       <Table sx={{ minWidth: 650 }} aria-label="simple table">
-//         <TableHead>
-//           <TableRow>
-//             <TableCell>Player_name</TableCell>
-//             <TableCell align="right">Player_ID</TableCell>
-//             <TableCell align="right">totalGames</TableCell>
-//             <TableCell align="right">succes_rate</TableCell>
-//           </TableRow>
-//         </TableHead>
-//         <TableBody>
-//           {rows.map((row) => (
-//             <TableRow
-//               key={row.Player_name}
-//               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-//             >
-//               <TableCell component="th" scope="row">
-//                 {row.Player_name}
-//               </TableCell>
-//               <TableCell align="right">{row.Player_ID}</TableCell>
-//               <TableCell align="right">{row.totalGames}</TableCell>
-//               <TableCell align="right">{row.succes_rate}</TableCell>
-//             </TableRow>
-//           ))}
-//         </TableBody>
-//       </Table>
-//     </TableContainer>
-//   );
-// }
