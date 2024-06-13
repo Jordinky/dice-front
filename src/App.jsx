c9fbc303f75fb5d28e2458e724ee4bd4c1fa7ad9
Jordi Pelaez <jordipelaez05@gmail.com>
2024-06-13 12:38:54 +0200

    added welcome message

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import { Container, Box } from '@mui/material';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Dice from './components/Dice'
+import Welcome from './components/Welcome';
 
 
 function App() {
   return (
     <ThemeToggle>
-      <Navbar/>
-      <Container component="main" sx={{ mt: 8, mb: 2 }}>
+      <Navbar/>     
+      <Container component="main" sx={{ mt: 3, mb: 1 }}>
+        <Welcome></Welcome>
           <Dice/>
       </Container>
       <Box display="flex" justifyContent="center" alignItems="center">
         <Footer />
       </Box>
-
     </ThemeToggle>
   );
 }
